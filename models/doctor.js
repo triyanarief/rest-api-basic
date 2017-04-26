ccec46827fa2ca1dc2d79f4966c113c725c109ac
triyanarief <triyanarief@icloud.com>
2017-04-26 16:43:12 +0700

    models & schema

---
+++ models/doctor.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// create doctor Schema & model
+const DoctorSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Name field is required']
+    },
+    rank: {
+        type: String
+    }
+    available: {
+        type: Boolean,
+        default: false
+    }
+    // add in geo location
+});
+
+const Doctor = mongoose.model('doctor', DoctorSchema);
+
+module.exports = Doctor;
