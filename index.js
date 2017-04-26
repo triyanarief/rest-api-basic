74539bc576164591a0f9f5c656c545bff6385420
triyanarief <triyanarief@icloud.com>
2017-04-26 21:30:03 +0700

    error handling

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ app.use(bodyParser.json());
 // initialize routes
 app.use('/api', routes);
 
+// error handling middleware
+app.use((err, req, res, next) => {
+    console.log(err); // to see properties of message in our console
+    res.status(422).send({error: err.message});
+});
+
 // listen for requests
 app.listen(process.env.port || 4000, () => {
     console.log('now listening for requests');
--- routes/api.js
+++ routes/api.js
@@ -3,24 +3,24 @@ const router = express.Router();
 const Doctor = require('../models/doctor');
 
 // get a list of doctor from the db
-router.get('/doctors', (req, res) => {
+router.get('/doctors', (req, res, next) => {
     res.send({type: 'GET'});
 });
 
 // add a new doctor to the db
-router.post('/doctors', (req, res) => {
+router.post('/doctors', (req, res, next) => {
   Doctor.create(req.body).then((doctor) => {
       res.send(doctor);
   });
 });
 
 // update a doctor in the db
-router.put('/doctors/:id', (req, res) => {
+router.put('/doctors/:id', (req, res, next) => {
     res.send({type: 'PUT'});
 });
 
 // delete a doctor from the db
-router.delete('/doctors/:id', (req, res) => {
+router.delete('/doctors/:id', (req, res, next) => {
     res.send({type: 'DELETE'});
 });
 
