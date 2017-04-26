d9721d7c24f2cc3766d09e9997d9e766ec84b392
triyanarief <triyanarief@icloud.com>
2017-04-26 12:36:49 +0700

    handling request

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require('express');
 // set up express app
 const app = express();
 
+app.get('/api', (req, res) => {
+  res.send({ name: 'Kinoy' });
+});
+
 // listen for requests
-app.listen(process.env.port || 4000, function(){
+app.listen(process.env.port || 4000, () => {
     console.log('now listening for requests');
 });
